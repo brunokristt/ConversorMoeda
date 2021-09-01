750809fbba5b496c8eee9b7f6b1bce965cfc49c9
brunokristt <brunokristt@gmail.com>
2021-09-01 17:01:43 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorMoeda
+{
+    class Calculadora
+    {
+        public static double Iof = 6.0;
+        public static double Calc(double cotacao, double quantidade)
+        {
+            double total = cotacao * quantidade;
+            return total = total + total * (Iof / 100);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConversorMoeda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conversor de moedas!");
+            Console.Write("Qual a atual cotação da moeda a ser comprada: ");
+            double cotacao = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine("Qual a quantidade que você deseja comprar: ");
+            double quantidade = int.Parse(Console.ReadLine());
+            
+            double result = Calculadora.Calc(cotacao, quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("O valor em reis é de " + result.ToString("F2",CultureInfo.InvariantCulture ));
+        }
+    }
+}
